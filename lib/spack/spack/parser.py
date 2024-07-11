2389c34035fc26317bba60296dffccf4db2f8382
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-07-11 16:30:51 +0200

    parser: parse compilers as direct build deps

--- lib/spack/spack/parser.py
+++ lib/spack/spack/parser.py
@@ -374,12 +374,11 @@ class SpecParser:
 class SpecNodeParser:
     """Parse a single spec node from a stream of tokens"""
 
-    __slots__ = "ctx", "has_compiler", "has_version", "literal_str"
+    __slots__ = "ctx", "has_version", "literal_str"
 
     def __init__(self, ctx, literal_str):
         self.ctx = ctx
         self.literal_str = literal_str
-        self.has_compiler = False
         self.has_version = False
 
     def parse(
@@ -426,23 +425,9 @@ class SpecNodeParser:
                 raise_parsing_error(str(e), e)
 
         while True:
-            if self.ctx.accept(TokenType.COMPILER):
-                if self.has_compiler:
-                    raise_parsing_error("Spec cannot have multiple compilers")
-
-                compiler_name = self.ctx.current_token.value[1:]
-                initial_spec.compiler = spack.spec.CompilerSpec(compiler_name.strip(), ":")
-                self.has_compiler = True
-
-            elif self.ctx.accept(TokenType.COMPILER_AND_VERSION):
-                if self.has_compiler:
-                    raise_parsing_error("Spec cannot have multiple compilers")
-
-                compiler_name, compiler_version = self.ctx.current_token.value[1:].split("@")
-                initial_spec.compiler = spack.spec.CompilerSpec(
-                    compiler_name.strip(), compiler_version
-                )
-                self.has_compiler = True
+            if self.ctx.accept(TokenType.COMPILER) or self.ctx.accept(TokenType.COMPILER_AND_VERSION):
+                build_dependency = self.ctx.current_token.value[1:]
+                initial_spec._add_dependency(spack.spec.Spec(build_dependency), depflag=spack.deptypes.BUILD, virtuals=())
 
             elif (
                 self.ctx.accept(TokenType.VERSION_HASH_PAIR)
